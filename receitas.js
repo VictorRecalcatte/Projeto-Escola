5c485b4aff99edce40731105814038ca1629b704
V1TUH <81694199+V1TUH@users.noreply.github.com>
2022-06-11 12:53:35 -0300

    Update receitas.js

--- receitas.js
+++ receitas.js
@@ -1,13 +1,14 @@
 function pizza_fingida(){
-  document.getElementById("teste").innerHTML = `<div id="conteudo">
+  document.getElementById("I.A").innerHTML = `<div id="conteudo">
   <div id="header">
     <div id="perfil" class="bot_icon">
       <img src="icon_bot.png" alt="" />
     </div>
-    <h3 id="perfil">Robson</h3>
+    <h3 id="perfil">Sustentabilidade</h3>
   </div>
-  <div id="receita">
+  <div id="dcr">
     <h2>Pizza Fingida</h2>
+    <img id="img"src="pizza.jpg" alt="" />
     <p><b>Ingredientes:</b></p>
     <p>*3 pães amanhecidos e fatiados</p>
     <p>*1 xícara de chá de molho de tomate </p>
@@ -24,21 +25,23 @@ function pizza_fingida(){
   </div>
   <div id="footer">
   <input id= "refresh"class="butão" type="button" value="Voltar" onClick="window.location.reload()">
+  
     
   </div>
 </div>`
 }
 
 function panquecas_verde(){
-  document.getElementById("teste").innerHTML = `<div id="conteudo">
+  document.getElementById("I.A").innerHTML = `<div id="conteudo">
   <div id="header">
     <div id="perfil" class="bot_icon">
       <img src="icon_bot.png" alt="" />
     </div>
-    <h3 id="perfil">Robson</h3>
+    <h3 id="perfil">Botizin</h3>
   </div>
-  <div id="receita">
+  <div id="dcr">
     <h2>Panquecas Verdes</h2>
+    <img id="img"src="panquecas.jpg" alt="" />
     <p><b>Ingredientes:</b></p>
     <p>*1 xícara de talos de brócolis</p>
     <p>*1 xícara de chá de leite</p>
@@ -55,27 +58,44 @@ function panquecas_verde(){
   </div>
   <div id="footer">
   <input id= "refresh"class="butão" type="button" value="Voltar" onClick="window.location.reload()">
+  
     
   </div>
 </div>`
 }
 
-/*Panquecas Verdes
+function bolinho_arroz(){
+  document.getElementById("I.A").innerHTML = `<div id="conteudo">
+  <div id="header">
+    <div id="perfil" class="bot_icon">
+      <img src="icon_bot.png" alt="" />
+    </div>
+    <h3 id="perfil">Botizin</h3>
+  </div>
+  <div id="dcr">
+    <h2>Bolinho de Arroz Assado</h2>
+    <img id="img"src="bolinho.jpg" alt="" />
+    <p><b>Ingredientes:</b></p>
+    <p>*2 xícaras de chá do arroz que sobrou do jantar ou almoço </p>
+    <p>*1 colher de sopa de cebola picada </p>
+    <p>*1 dente de alho picado  </p>
+    <p>*2 ovos</p>
+    <p>*2 colheres de sopa de salsinha picada  </p>
+    <p>*1 xícara de chá de farinha de trigo </p>
+    <br>
+    <p><b>Modo de preparo:</b></p>
+    <p>-Coloque todos os ingredientes num pote e misture com as mãos até formar uma massa homogênea </p>
+    <p>-Faça pequenos bolinhos e coloque-os num refratário untado com óleo.</p>
+    <p>-Asse em forno médio por 30 minutos.</p>
+    
 
-Ingredientes: 
+  </div>
+  <div id="footer">
 
-1 xícara de talos de brócolis 
+  <input id= "refresh"class="butão" type="button" value="Voltar" onClick="window.location.reload()">
+  
+    
+  </div>
+</div>`
+}
 
-1 xícara de chá de leite 
-
-¾ de xícara de chá de farinha de trigo 
-
-2 ovos 
-
-1 colher de sopa de margarina 
-
-Sal a gosto 
-
-Modo de preparo: 
-
-Bata os talos com o leite no liquidificador. Passe a mistura numa peneira e leve novamente ao liquidificador com o restante dos ingredientes. Unte uma frigideira com óleo e coloque a massa com o auxílio de uma concha. Deixe-a assar até obter uma cor dourada, vire-a e coloque o recheio de sua preferência.*/
